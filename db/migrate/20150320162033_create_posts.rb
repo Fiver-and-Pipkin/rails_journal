56a92d022f7946e76e1f9efa0dc2e8a0c86bbcb5
Debbie Cravey <cinemaestro@yahoo.com>
2015-03-20 09:35:58 -0700

    add RSpec and Capybara for testing

--- db/migrate/20150320162033_create_posts.rb
+++ db/migrate/20150320162033_create_posts.rb
@@ -1,6 +1,11 @@
 class CreatePosts < ActiveRecord::Migration
   def change
     create_table :posts do |t|
+      t.column :entry, :text
+      t.column :title, :string
+      t.column :date, :date
+
+      t.timestamps
     end
   end
 end
